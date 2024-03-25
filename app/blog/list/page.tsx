a799a566191be53807c2d483ca92ca7abf08584c
zhenyuWang <13641039885@163.com>
2024-03-25 23:10:11 +0800

    fix: list data cannot be updated automatically

--- app/blog/list/page.tsx
+++ app/blog/list/page.tsx
@@ -1,28 +1,38 @@
+'use client'
+
 import { fetchBlogs } from '../../lib/actions'
 
 import type { Blog } from '@/types/blog'
 
 import NavBar from '@/components/NavBar/NavBar'
 import BlogList from '@/components/Blog/List'
+import { useEffect, useState } from 'react'
 
-export default async function BlogListPage() {
-  const blogs = await fetchBlogs()
-  let blogList: Blog[] = []
+export default function BlogListPage() {
+  const [blogs, setBlogs] = useState<Blog[]>([])
 
-  if (Array.isArray(blogs)) {
-    blogList = blogs.map((blog) => {
-      return {
-        _id: blog._id.toString(),
-        title: blog.title,
-        content: blog.content,
-        publishDate: blog.publishDate,
+  useEffect(() => {
+    async function getBlogs() {
+      const blogs = await fetchBlogs()
+      if (Array.isArray(blogs)) {
+        setBlogs(
+          blogs.map((blog) => {
+            return {
+              _id: blog._id.toString(),
+              title: blog.title,
+              content: blog.content,
+              publishDate: blog.publishDate,
+            }
+          }),
+        )
       }
-    })
-  }
+    }
+    getBlogs()
+  }, [])
   return (
     <>
       <NavBar />
-      <BlogList list={blogList} />
+      <BlogList list={blogs} />
     </>
   )
 }
